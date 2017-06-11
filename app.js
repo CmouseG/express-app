1bb873da61eb4f6e24c2fed95bcaab80e8dbb26c
CmouseG <1668924806@qq.com>
2017-06-11 11:24:55 -0700

    redis

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/redis', redis_r);
+// app.use('/redis', redis_r);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
