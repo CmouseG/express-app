c50bbdb5e684b9d298991a2258f9c7d5b069022a
CmouseG <1668924806@qq.com>
2017-06-11 10:30:08 -0700

    redis

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var redis_r = require('./routes/redis');
+// var redis_r = require('./routes/redis');
 
 var redis = require('redis');
 var RedisStore = require('connect-redis')(express);
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/redis', redis_r);
+// app.use('/redis', redis_r);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
